e4b64d8fb77ebaa32bc282bb63cbaa84ee7153fd
Anna Savina <anna.s.savina@gmail.com>
2019-03-21 20:01:57 +0300

    WebRequest, WebResponse

--- MetanitSharpNet/Requests.cs
+++ MetanitSharpNet/Requests.cs
@@ -29,6 +29,18 @@ namespace MetanitSharpNet
                     case 'a':
                         usingWebClientAsync();
                         break;
+                    case 'q':
+                        usingWebRequest();
+                        break;
+                    case 'r':
+                        usingHttpWebRequest();
+                        break;
+                    case 'c':
+                        requestCredentials();
+                        break;
+                    case 'h':
+                        responseHeaders();
+                        break;
                     case 'x': return;
                 }
                 Console.ReadKey();
@@ -40,6 +52,10 @@ namespace MetanitSharpNet
             Console.WriteLine("Нажмите клавишу для вывода информации");
             Console.WriteLine("W - загрузка файла через WebClient");
             Console.WriteLine("A - асинхронная загрузка файла");
+            Console.WriteLine("Q - WebRequest");
+            Console.WriteLine("R - HttpWebRequest");
+            Console.WriteLine("C - credentials");
+            Console.WriteLine("H - headers");
         }
 
         static void usingWebClient()
@@ -72,6 +88,66 @@ namespace MetanitSharpNet
         {
             WebClient client = new WebClient();
             await client.DownloadFileTaskAsync(new Uri("http://uploads.athn.ru/sometext.txt"), "mytxtFile.txt");
+        }
+
+        static void usingWebRequest()
+        {
+            WebRequest request = WebRequest.Create("http://uploads.athn.ru/sometext.txt");
+            WebResponse response = request.GetResponse();
+            using (Stream stream = response.GetResponseStream())
+            {
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    string line = "";
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+            response.Close();
+            Console.WriteLine("Запрос выполнен");
+        }
+
+        static async void usingHttpWebRequest()
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://uploads.athn.ru/sometext.txt");
+            HttpWebResponse response = (HttpWebResponse)await request.GetResponseAsync();
+            using (Stream stream = response.GetResponseStream())
+            {
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    Console.WriteLine(reader.ReadToEnd());
+                }
+            }
+            response.Close();
+        }
+
+        static async void requestCredentials()
+        {
+            Console.WriteLine("Try this example with valid Url!");
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://somesite.com/auth");
+                request.Credentials = new NetworkCredential("myLogin", "myPassword");
+                HttpWebResponse response = (HttpWebResponse)await request.GetResponseAsync();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        static async void responseHeaders()
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://uploads.athn.ru/sometext.txt");
+            HttpWebResponse response = (HttpWebResponse)await request.GetResponseAsync();
+            WebHeaderCollection headers = response.Headers;
+            for (int i = 0; i < headers.Count; i++)
+            {
+                Console.WriteLine("{0}: {1}", headers.GetKey(i), headers[i]);
+            }
+            response.Close();
         }
     }
 }
