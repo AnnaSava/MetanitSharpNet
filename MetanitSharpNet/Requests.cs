4e3412ef08fe426dd403c679ed883d45bafd070d
Anna Savina <anna.s.savina@gmail.com>
2019-03-23 15:46:37 +0300

    Added MVC project, POST

--- MetanitSharpNet/Requests.cs
+++ MetanitSharpNet/Requests.cs
@@ -41,6 +41,9 @@ namespace MetanitSharpNet
                     case 'h':
                         responseHeaders();
                         break;
+                    case 'p':
+                        postRequest();
+                        break;
                     case 'x': return;
                 }
                 Console.ReadKey();
@@ -56,6 +59,7 @@ namespace MetanitSharpNet
             Console.WriteLine("R - HttpWebRequest");
             Console.WriteLine("C - credentials");
             Console.WriteLine("H - headers");
+            Console.WriteLine("P - отправка данных в POST-запросе");
         }
 
         static void usingWebClient()
@@ -148,6 +152,42 @@ namespace MetanitSharpNet
                 Console.WriteLine("{0}: {1}", headers.GetKey(i), headers[i]);
             }
             response.Close();
+        }
+
+        static async void postRequest()
+        {
+            await PostRequestAsync();
+        }
+
+        private static async Task PostRequestAsync()
+        {
+            WebRequest request = WebRequest.Create("http://localhost:1755/Home/PostData");
+            request.Method = "POST"; // для отправки используется метод Post
+                                     // данные для отправки
+            string data = "sName=Hello world!";
+            // преобразуем данные в массив байтов
+            byte[] byteArray = System.Text.Encoding.UTF8.GetBytes(data);
+            // устанавливаем тип содержимого - параметр ContentType
+            request.ContentType = "application/x-www-form-urlencoded";
+            // Устанавливаем заголовок Content-Length запроса - свойство ContentLength
+            request.ContentLength = byteArray.Length;
+
+            //записываем данные в поток запроса
+            using (Stream dataStream = request.GetRequestStream())
+            {
+                dataStream.Write(byteArray, 0, byteArray.Length);
+            }
+
+            WebResponse response = await request.GetResponseAsync();
+            using (Stream stream = response.GetResponseStream())
+            {
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    Console.WriteLine(reader.ReadToEnd());
+                }
+            }
+            response.Close();
+            Console.WriteLine("Запрос выполнен...");
         }
     }
 }
